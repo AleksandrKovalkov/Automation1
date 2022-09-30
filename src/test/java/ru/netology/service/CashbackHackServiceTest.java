16edeef6fbc93a64e39dee5087ff517c40ae1d70
Ковальков Александр <akovalkoff@yandex.ru>
2022-09-30 12:26:05 +0300

    junit4

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,14 +1,14 @@
 package ru.netology.service;
 
-import org.junit.Test;
 import org.junit.Assert;
+import org.junit.Test;
 
 public class CashbackHackServiceTest {
 
     CashbackHackService service = new CashbackHackService();
 
     @Test
-    void shouldReturn400IfAmountIs600(){
+    public void shouldReturn400IfAmountIs600() {
         int amount = 600;
         int actual = service.remain(amount);
         int expected = 400;
@@ -17,31 +17,31 @@ public class CashbackHackServiceTest {
     }
 
     @Test
-    void shouldReturn500IfAmountIs1500(){
+    public void shouldReturn500IfAmountIs1500() {
         int amount = 1500;
         int actual = service.remain(amount);
         int expected = 500;
 
-        Assert.assertEquals(actual,expected);
+        Assert.assertEquals(actual, expected);
     }
 
 
     @Test
-    void shouldReturn1000IfAmountIs0(){
+    public void shouldReturn1000IfAmountIs0() {
         int amount = 0;
         int actual = service.remain(amount);
         int expected = 1000;
 
-        Assert.assertEquals(actual,expected);
+        Assert.assertEquals(actual, expected);
     }
 
     @Test
-    void shouldReturn0IfAmountIs1000(){
+    public void shouldReturn0IfAmountIs1000() {
         int amount = 1000;
         int actual = service.remain(amount);
         int expected = 0;
 
-        Assert.assertEquals(actual,expected);
+        Assert.assertEquals(actual, expected);
     }
 
 }
